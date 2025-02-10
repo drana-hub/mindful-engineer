b662897ec4e1c3d6e2b00433f4f1a232a714af7a
Divyansh Rana <divyanshr@Divyanshs-MacBook-Air.local>
2025-02-10 23:20:44 +0530

    First commit in the frontend repo

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      Mindful Engineer
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import Header from "../components/Header";
+import axios from "axios";
+import "../styles/Home.css";
+
+const Home = () => {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("/api/message").then((response) => {
+      setMessage(response.data.message);
+    });
+  }, []);
+
+  return (
+    <div className="container">
+      <Header />
+      <p className="message">{message}</p>
+    </div>
+  );
+};
+
+export default Home;
